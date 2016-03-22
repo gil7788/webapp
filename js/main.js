a9c64b330d70a8d1721885154a3b44d70c098fb7
Gil Henkin <gil7788@gmail.com>
2016-03-22 18:14:59 +0200

    implement all tabs,form error alert,search and read data from config file

--- js/main.js
+++ js/main.js
@@ -1,165 +1,394 @@
 "use strict";
 var currentTab = 0;
-var settingsIconIsClicked = true;
-function setPage() {
-  /*Set tabs*/
-  //setTabs();
-  /*Set first content tab*/
-  setFirstTab();
-  /*Set second content tab*/
-  setSecondTab();
-  /*Set third content tab*/
-  setThirdTab();
-  /*Set fourth content tab*/
-  setFourthTab();
-  /*Set help bar*/
-  updateHelpBar();
-}
+var panels = [document.getElementsByClassName("panel")[0], document.getElementsByClassName("panel")[1]];
+var helpBarState = [{
+  selectionList: false,
+  settings: true,
+  expand: false,
+  panel: true
+}, {
+  selectionList: false,
+  settings: false,
+  expand: true,
+  panel: false
+}, {
+  selectionList: false,
+  settings: true,
+  expand: false,
+  panel: true
+}, {
+  selectionList: false,
+  settings: false,
+  expand: true,
+  panel: false
+}]
+var myData = {
+  data: [
+    [{
+      name: '',
+      url: ''
+    }, {
+      name: '',
+      url: ''
+    }, {
+      name: '',
+      url: ''
+    }],
+    [{
+      name: '',
+      url: ''
+    }, {
+      name: '',
+      url: ''
+    }, {
+      name: '',
+      url: ''
+    }]
+  ]
+};
 
-function setTabs() {
+function setPage() {
   currentTab = 0;
-  var tabs = document.getElementsByClassName("tabs")[0];
-  for (var i = 0; i < 4; i++) {
-    var tab = tabs.getElementsByTagName("li")[i];
-    var link = tab.getElementsByTagName("a")[0];
-    if (emptyClassList(tab) && i == 0) {
-      tab.className = "grey";
-      link.className = "blackText";
-    } else {
-      tab.className = "darkGrey";
-      link.className = "whiteText";
+  /*Set help bar*/
+  getConfigData();
+  getLocalData();
+  setInput();
+
+  function setInput() {
+    /*setReport(0,0,"Ynet", "http://ynet.com");
+    setReport(0,1,"Walla", "http://walla.com");
+    setReport(0,2,"Isreali Ynet", "http://ynet.co.il");
+
+    setReport(1,0,"My Comverse1!!", "http://gil7788.github.io/comverse/");*/
+
+    for (var i = 0; i < 2; i++) {
+      var panel = document.getElementsByClassName("panel")[i];
+      for (var j = 0; j < 3; j++) {
+        var report = panel.getElementsByClassName("report")[j];
+        var name = report.getElementsByTagName("input")[0];
+        var url = report.getElementsByTagName("input")[1];
+        name.setAttribute('value', myData.data[i][j].name);
+        url.setAttribute('value', myData.data[i][j].url);
+      }
+    }
+
+    function setReport(panel, report, name, url) {
+      panels[panel].getElementsByTagName("input")[report * 2].setAttribute("value", name);
+      panels[panel].getElementsByTagName("input")[report * 2 + 1].setAttribute("value", url);
+    }
+
+  }
+
+  function getConfigData() {
+    function done(res) {
+      if (!UTILS.isObject(res)) {
+        res = JSON.parse(res);
+      }
+      getNotification(res);
+      getQuickActions(res);
+    }
+    var options = {
+      method: "GET",
+      done: done
+    };
+    var config = UTILS.ajax("data/config.json", options);
+
+    function getNotification(res) {
+      var notificationsContent = document.getElementsByClassName("notificationsContent")[0];
+      notificationsContent.innerHTML = res.notification;
+    }
+
+    function getQuickActions(res) {
+      for (var i = 0; i < 3; i++) {
+        getQuickAction(res, i);
+      }
+
+      function getQuickAction(res, i) {
+        var action = res.quickActions[i];
+        var nav = document.getElementsByClassName("nav-section")[i];
+        /*Get label */
+        var label = nav.getElementsByTagName("p")[0];
+        label.innerHTML = action.label;
+        /*Get icon */
+        var icon = "url('../img/icons/" + action.icon + ".png')";
+        nav.style.backgroundImage = icon;
+        /*Get actions label */
+        var actionsLabel = document.getElementsByClassName("menu-caption")[i].getElementsByTagName("p")[0];
+        actionsLabel.innerHTML = action.actionsLabel;
+        /*Get actions*/
+        var actionList = document.getElementsByClassName("action-list")[i];
+        for (var i = 0; i < action.actions.length; i++) {
+          var listElement = document.createElement("li");
+          actionList.appendChild(listElement);
+          var name = action.actions[i].label;
+          var url = action.actions[i].url;
+          var link = document.createElement("a");
+          link.innerHTML = name;
+          link.href = url;
+          listElement.appendChild(link);
+        }
+      }
+    }
+  }
+
+  function getLocalData() {
+    var temp = localStorage.getItem('data');
+    if (temp !== undefined && temp !== null) {
+      myData = JSON.parse(temp);
     }
   }
 }
 /*Setting functions*/
-function setFirstTab(){
-  var iframe = document.getElementById("quick-reports").getElementsByTagName("iframe")[0];
-  hide(iframe);
-}
-function setSecondTab(){
-  var tabContent = document.getElementById("my-folders");
-  hide(tabContent);
-}
-function setThirdTab(){
-  var tabContent = document.getElementById("my-team-folders");
-  hide(tabContent);
-}
-function setFourthTab(){
-  var tabContent = document.getElementById("public-folders");
-  hide(tabContent);
-}
-function updateHelpBar(){
-  var bar = document.getElementsByClassName("helpBar")[0];
-  var settingsIcon = document.getElementsByClassName("settings")[0];
-  var expendIcon = document.getElementsByClassName("expand")[0];
+function updateHelpBar() {
+  if (currentTab == 0 || currentTab == 2) {
+    displayElement("selectionList");
+    displayElement("settings");
+    displayElement("panel");
+  }
+  displayElement("expand");
 
-  if(currentTab == 0 || currentTab == 2){
-    show(settingsIcon);
-    if(contains(settingsIcon,"white")){
-      hide(expendIcon);
-    }
-    else{
-      show(expendIcon);
-    }
-    if(settingsIconIsClicked == true){
-      remove(settingsIcon,"grey");
-      add(settingsIcon,"white");
-    }
-    else{
-      remove(settingsIcon,"white");
-      add(settingsIcon,"grey");
+  function displayElement(state) {
+    var tab = document.getElementsByClassName("tabContent")[currentTab];
+    var htmlState = tab.getElementsByClassName(state)[0];
+    if (helpBarState[currentTab][state] == true) {
+      show(htmlState);
+    } else {
+      hide(htmlState);
     }
   }
-  if(currentTab == 1 || currentTab == 3){
-    hide(settingsIcon);
-    show(expendIcon);
-  }
 }
-
 /*Setting functions-End*/
-
-
-
 function changeTab(i) {
   var oldTab = currentTab;
   currentTab = i;
-  if(oldTab != currentTab){
+  if (oldTab != currentTab) {
     uncheckTab(oldTab);
     checkTab(currentTab);
+    updateHelpBar();
   }
-  updateHelpBar();
-  function uncheckTab(i) {
+
+  function uncheckTab(tabToUncheck) {
     var tabs = document.getElementsByClassName("tabs")[0];
-    var tab = tabs.getElementsByTagName("li")[i];
+    var tab = tabs.getElementsByTagName("li")[tabToUncheck];
     var link = tab.getElementsByTagName("a")[0];
     tab.className = "darkGrey";
     link.className = "whiteText";
-    var tabContent = document.getElementsByClassName("tabContent")[i];
+    var tabContent = document.getElementsByClassName("tabContent")[tabToUncheck];
     hide(tabContent);
   }
-  function checkTab(i){
+
+  function checkTab(i) {
     var tabs = document.getElementsByClassName("tabs")[0];
     var tab = tabs.getElementsByTagName("li")[i];
     var link = tab.getElementsByTagName("a")[0];
-    if (contains(tab, "darkGrey")) {
-      tab.className = "grey";
-      link.className = "blackText";
-    }
+    tab.className = "grey";
+    link.className = "blackText";
     var tabContent = document.getElementsByClassName("tabContent")[i];
     show(tabContent);
   }
 }
-function toggleSettingsIcon(){
-  if(settingsIconIsClicked == true)
-    settingsIconIsClicked = false;
-  else
-    settingsIconIsClicked = true;
+/*paint the settings color*/
+function toggleSettingsIcon() {
+  var tab = document.getElementsByClassName("tabContent")[currentTab];
+  var settingsIcon = tab.getElementsByClassName("settings")[0];
+  if (helpBarState[currentTab].panel == true) {
+    remove(settingsIcon, "white");
+    add(settingsIcon, "grey");
+    helpBarState[currentTab].panel = false;
+  } else {
+    remove(settingsIcon, "grey");
+    add(settingsIcon, "white");
+    helpBarState[currentTab].panel = true;
+  }
   updateHelpBar();
 }
-function expand(){
+
+function showPageInNewTab() {
+  var tab = document.getElementsByClassName("tabContent")[currentTab];
+  var url = tab.getElementsByClassName("iframe")[0].src;
+  var expandIcon = tab.getElementsByClassName("expand")[0];
+  expandIcon.setAttribute("href", url);
+}
+
+function expand() {
   /*get url from Iframe*/
-  var url =  document.getElementsByClassName("tabContent")[currentTab].getElementsByClassName("iframe")[0].contentWindow.location.href;
+  var url = document.getElementsByClassName("tabContent")[currentTab].getElementsByClassName("iframe")[0].contentWindow.location.href;
   var link = document.getElementsByClassName("expand")[0].setAttribute('href', url);
   //window.open(url, '_blank');
 }
-function submit(){
-  if(isValidForm()){
-    getUrlLink(1);
-  }
-  else{
-
-  }
-  function isValidForm(){
-      for(var i=0;i<3;i++){
-        if(!isValidReport(i)){
-          return false;
-        }
-      }
-      return true;
-  }
-  function isValidReport(i){
-    var name = document.getElementsByClassName("report")[i].getElementsByTagName("input")[0].value;
-    var url = document.getElementsByClassName("report")[i].getElementsByTagName("input")[1].value;
-
-    if(name.length > 0 && url.length == 0 || name.length == 0 && url.length > 0){
-      return false;
+/*Not finished*/
+function submit() {
+  var settingsIcon = document.getElementsByClassName("settings")[0];
+  var tab = document.getElementsByClassName("tabContent")[currentTab];
+  var urls = tab.getElementsByClassName("urls")[0];
+  var iframe = document.getElementsByClassName("iframe")[currentTab];
+  if (isValidForm()) {
+    getUrlLinks();
+    if (urls.innerHTML != '') {
+      showPageInIframe();
+      toggleSettingsIcon();
+      helpBarState[currentTab].expand = true;
+      helpBarState[currentTab].selectionList = true;
+      helpBarState[currentTab].panel = false;
+      updateHelpBar();
+      show(iframe);
+    } else {
+      helpBarState[currentTab].selectionList = false;
+      helpBarState[currentTab].expand = false;
+      helpBarState[currentTab].panel = true;
+      updateHelpBar();
+      hide(iframe);
     }
-    var expresion = "^(http[s]?:\\/\\/(www\\.)?|ftp:\\/\\/(www\\.)?|www\\.){1}([0-9A-Za-z-\\.@:%_\+~#=]+)+((\\.[a-zA-Z]{2,3})+)(/(.)*)?(\\?(.)*)?";
-    var exp = new RegExp(expresion);
-    if(!url.match(exp) && ! url.length == 0){
-      return false;
+    saveData();
+  }
+
+  function isValidForm() {
+    for (var i = 0; i < 3; i++) {
+      if (!isValidReport(i)) {
+        return false;
+      }
     }
     return true;
-  }
-  function getUrlLink(i){
-    var name = document.getElementsByClassName("report")[i].getElementsByTagName("input")[0].value;
-    var url = document.getElementsByClassName("report")[i].getElementsByTagName("input")[1].value;
-    var selectionList = document.getElementsByClassName("selectionList")[0];
 
-    var fragment = document.createElement("option");
-    fragment.innerHTML = name;
-    // You can use native DOM methods to insert the fragment:
-    fragment.insertAfter(fragment, selectionList.lastChild);
+    function isValidReport(i) {
+      var nameInput = tab.getElementsByClassName("report")[i].getElementsByTagName("input")[0];
+      var urlInput = tab.getElementsByClassName("report")[i].getElementsByTagName("input")[1];
+      var name = nameInput.value;
+      var url = urlInput.value;
+      var expresion = "^(http[s]?:\\/\\/(www\\.)?|ftp:\\/\\/(www\\.)?|www\\.){1}([0-9A-Za-z-\\.@:%_\+~#=]+)+((\\.[a-zA-Z]{2,3})+)(/(.)*)?(\\?(.)*)?";
+      var exp = new RegExp(expresion);
+
+      /*hide old errors*/
+      var error0 = document.getElementsByClassName("error")[i * 3];
+      var error1 = document.getElementsByClassName("error")[i * 3 + 1];
+      var error2 = document.getElementsByClassName("error")[i * 3 + 2];
+      hide(error0);
+      hide(error1);
+      hide(error2);
+      remove(nameInput, "redBorder");
+      remove(urlInput, "redBorder");
+
+      if (name.length == 0 && url.length > 0) {
+        var error = document.getElementsByClassName("error")[i * 3];
+        show(error);
+        add(nameInput, "redBorder");
+        return false;
+      } else if (name.length > 0 && url.length == 0) {
+        var error = document.getElementsByClassName("error")[i * 3 + 1];
+        show(error);
+        add(urlInput, "redBorder");
+        return false;
+      } else if (!url.match(exp) && !url.length == 0) {
+        var error = document.getElementsByClassName("error")[i * 3 + 2];
+        show(error);
+        add(urlInput, "redBorder");
+        return false;
+      }
+      return true;
+    }
+  }
+
+  function getUrlLinks() {
+    urls.innerHTML = '';
+    for (var i = 0; i < 3; i++) {
+      getUrlLink(i);
+    }
+
+    function getUrlLink(i) {
+      if (!emptyReport(i)) {
+        var name = tab.getElementsByClassName("report")[i].getElementsByTagName("input")[0].value;
+        var url = tab.getElementsByClassName("report")[i].getElementsByTagName("input")[1].value;
+        var urls = tab.getElementsByClassName("urls")[0];
+        /*Create a new option*/
+        var optionTag = document.createElement("option");
+        var urlName = document.createTextNode(name);
+        optionTag.appendChild(urlName);
+        urls.appendChild(optionTag);
+        /*Create hidden element with url under its option tag*/
+        var pTag = document.createElement("p");
+        pTag.className = "hide";
+        pTag.setAttribute("value", i);
+        var urlValue = document.createTextNode(url);
+        pTag.appendChild(urlValue);
+        urls.appendChild(pTag);
+      }
+
+      function emptyReport(i) {
+        var name = tab.getElementsByClassName("report")[i].getElementsByTagName("input")[0].value;
+        var url = tab.getElementsByClassName("report")[i].getElementsByTagName("input")[1].value;
+        return name.length == 0 && url.length == 0;
+      }
+    }
+  }
+
+  function saveData() {
+    /**/
+    for (var i = 0; i < 2; i++) {
+      var panel = document.getElementsByClassName("panel")[i];
+      for (var j = 0; j < 3; j++) {
+        var report = panel.getElementsByClassName("report")[j];
+        var name = report.getElementsByTagName("input")[0].value;
+        var url = report.getElementsByTagName("input")[1].value;
+        myData.data[i][j].name = name;
+        myData.data[i][j].url = url;
+      }
+    }
+    localStorage.setItem("data", JSON.stringify(myData));
+  }
+}
+
+function showPageInIframe() {
+  var tab = document.getElementsByClassName("tabContent")[currentTab];
+  /*select*/
+  var urls = tab.getElementsByClassName("urls")[0];
+  /*url index in select*/
+  var index = urls.selectedIndex;
+  /*The url itself*/
+  var url = urls.getElementsByTagName("p")[index].innerHTML;
+  /*Iframe*/
+  var iframe = tab.getElementsByClassName("iframe")[0];
+  iframe.setAttribute('src', url);
+  show(iframe);
+}
+
+function search(e) {
+  if (e.keyCode == 13) {
+    e.preventDefault();
+    var searchString = document.getElementsByClassName("search-box")[0].getElementsByTagName("input")[0].value;
+    getAllUrls();
+    function getAllUrls() {
+      if(helpBarState[0].selectionList){
+        var panel = document.getElementsByClassName("panel")[0];
+        var selectionList = document.getElementsByClassName("urls")[0];
+          for (var j = 0; j < selectionList.options.length; j++) {
+            var report = panel.getElementsByClassName("report")[j];
+            var name = report.getElementsByTagName("input")[0].value;
+            var p = name.search(searchString) > -1;
+            debugger;
+            if (p) {
+              selectionList.selectedIndex = j;
+              changeTab(0);
+              showPageInIframe();
+            }
+          }
+      }
+      if(helpBarState[2].selectionList){
+        var panel = document.getElementsByClassName("panel")[1];
+        var selectionList = document.getElementsByClassName("urls")[1];
+          for (var j = 0; j < selectionList.options.length; j++) {
+            var report = panel.getElementsByClassName("report")[j];
+            var name = report.getElementsByTagName("input")[0].value;
+            var p = name.search(searchString) > -1;
+            debugger;
+            if (p) {
+              selectionList.selectedIndex = j;
+              changeTab(2);
+              showPageInIframe();
+            }
+          }
+      }
+
+    }
   }
 }
 /***********Utilities****************/
@@ -195,26 +424,28 @@ function containsOneClass(element, classes) {
   return false;
 };
 /*DOM element*/
-function show(element){
-  remove(element," hide");
+function show(element) {
+  remove(element, " hide");
 }
 /*DOM element*/
-function hide(element){
-  add(element,"hide");
+function hide(element) {
+  add(element, "hide");
 }
 
-function remove(element,removeClass){
-  if(contains(element," "+removeClass))
-    element.className = element.className.replace(" "+removeClass , '' );
+function remove(element, removeClass) {
+  if (contains(element, " " + removeClass))
+    element.className = element.className.replace(" " + removeClass, '');
   else
-    element.className = element.className.replace(removeClass , '' );
+    element.className = element.className.replace(removeClass, '');
 }
-function add(element,addClass){
-  if(!contains(element,addClass)){
-      element.className += " "+addClass;
+
+function add(element, addClass) {
+  if (!contains(element, addClass)) {
+    element.className += " " + addClass;
   }
 }
-function replace(element,addClass){
+
+function replace(element, addClass) {
   element.className = addClass;
 }
 /***********Utilities****************/
